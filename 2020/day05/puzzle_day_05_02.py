6b41fb37afc8f4a652ae78da09da3221f1d1e3ee
Matt Johnson <>
2020-12-05 06:20:08 -0500

    finished day 5 and set up day 6

--- 2020/day05/puzzle_day_05_02.py
+++ 2020/day05/puzzle_day_05_02.py
@@ -1,8 +1,10 @@
 filepath = 'input05'
 seats = []
 
+tmpSeatIds = []
 tmpSeatId = 0
 maxSeatId = 0
+missingSeatId = 0
 
 with open(filepath) as fp:
    line = fp.readline()
@@ -10,6 +12,7 @@ with open(filepath) as fp:
    while line:
        seats.append(line)
        line = fp.readline()
+
 
 for seat in seats:
     rowPos = 0;
@@ -29,8 +32,29 @@ for seat in seats:
         #print(colPos)
 
     tmpSeatId = rowPos * 8 + colPos
+    tmpSeatIds.append(tmpSeatId)
     #print(tmpSeatId)
     if tmpSeatId > maxSeatId:
         maxSeatId = tmpSeatId
 
+tmpSearchIds = []
+missingIds = []
+for i in range(1,127):
+    for ii in range (0,8):
+        tmpSearchId = i * 8 + ii
+        #print(tmpSearchId)
+        tmpSearchIds.append(tmpSeatId)
+        if tmpSearchId not in tmpSeatIds:
+            missingIds.append(tmpSearchId)
+
+for missingId in missingIds:
+    if (missingId - 1) in tmpSeatIds:
+        if (missingId + 1) in tmpSeatIds:
+            print(missingId)
+
+print("tmpSearchIds size: {}".format(len(tmpSearchIds)))
+print("tmpSeatIds size: {}".format(len(tmpSeatIds)))
+
+
+print(missingSeatId)
 print(maxSeatId)
