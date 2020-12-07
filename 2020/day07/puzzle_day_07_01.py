5f2641c673e83c646350dd7775479d222b69f1e8
Matt Johnson <>
2020-12-07 03:49:06 -0500

    day 7 first approach

--- 2020/day07/puzzle_day_07_01.py
+++ 2020/day07/puzzle_day_07_01.py
@@ -1,3 +1,4 @@
+import re
 filepath = 'test'
 seats = []
 
@@ -8,23 +9,69 @@ evalLine = ""
 uniqueQ = ""
 uniqueQTot = 0
 
+rules = []
+rule = []
+
+matches = 0
+
+MATCH_STRING = "shiny gold"
+
+deadEndColors = []
+
+def IsGoldBagInside(color):
+    matchFound = False
+    if color not in deadEndColors:
+        print("----Color to search: {}".format(color))
+        for rule in rules:
+            if color in rule[0]:
+                if "no " not in rule[1]:
+                    for i in range(1,len(rule)):
+                        if MATCH_STRING in rule[i]:
+                            matchFound = True
+                            break
+                        else:
+                            color = rule[i][2:rule[i].find("bag")]
+                            matchFound = IsGoldBagInside(color)
+                    if matchFound:
+                        break
+    return matchFound
+
+
+
+
 
 with open(filepath) as fp:
    line = fp.readline()
-
    while line:
-       if(line.isspace()):
-           print(evalLine)
-           for c in evalLine:
-               if(c.isalpha()):
-                   if c not in uniqueQ:
-                       uniqueQ += c
-           uniqueQTot += len(uniqueQ)
-           print(len(uniqueQ))
-           evalLine = ""
-           uniqueQ = ""
-
-       evalLine += line
+       tmpRule = line.split("\n")
+       tmpRule = tmpRule[0].split(" contain ")
+       rule.append(tmpRule[0])
+       tmpRule = tmpRule[1].split(", ")
+       for innerBag in tmpRule:
+           rule.append(innerBag)
+       rules.append(rule)
+       rule = []
        line = fp.readline()
 
-print(uniqueQTot)
+   for rule in rules:
+       print("Rule to search: {}".format(rule))
+       matchFound = False
+       if MATCH_STRING not in rule[0]:
+           if "no" not in rule[1]:
+               for i in range(1,len(rule)):
+                   if MATCH_STRING in rule[i]:
+                       # print(rule[i])
+                       matchFound = True
+                       break
+                   else:
+                       color = rule[i][2:rule[i].find("bag")]
+                       matchFound = IsGoldBagInside(color)
+                       if matchFound == False:
+                           deadEndColors.append(color)
+
+       if(matchFound):
+           matches += 1
+
+
+
+print(matches)
