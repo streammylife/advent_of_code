d904aef7e656a9246cfa3b820598da82d1415f3e
Matt Johnson <>
2020-12-09 06:08:37 -0500

    finished day9 part 1

--- 2020/day09/puzzle_day_09_01.py
+++ 2020/day09/puzzle_day_09_01.py
@@ -1,91 +1,35 @@
-import re
-import time
+filepath = 'test'
+numbers = []
+window = []
+windowLen = 5
+invalidNum = 127
 
-filepath = 'input'
-pc = 0
-operations = []
-accumulator = 0
-executed = []
-switched = []
-
-loopComplete = False
-
-def ClearExecuted():
-    for i in range(0,len(executed)):
-        executed[i] = False
-
-def RunLoop(operations):
-    returnVal = [0,0]
-    pc = 0
-    loopComplete = False
-    accumulator = 0
-    ClearExecuted()
-    while loopComplete == False:
-        if pc == len(operations):
-            loopComplete = True
-            print(accumulator)
-            print("END")
-        else:
-            if executed[pc] == False:
-                #print(operations[pc])
-                opType = operations[pc][:3]
-                sign = operations[pc][4]
-                val = int(operations[pc][5:])
-                # print(opType)
-                # print(sign)
-                # print(val)
-                # print("----")
-
-                executed[pc] = True
-
-                if opType == "nop":
-                    pc = pc + 1
-                elif opType == "acc":
-                    pc = pc + 1
-                    if sign == "+":
-                        accumulator = accumulator + val
-                    else:
-                        accumulator = accumulator - val
-                elif opType == "jmp":
-                    if sign == "+":
-                        pc = pc + val
-                    else:
-                        pc = pc - val
-            else:
-                loopComplete = True
-
-    return pc
-
+# windowLen = 25
+# invalidNum = 57195069
 with open(filepath) as fp:
    line = fp.readline()
    cnt = 1
    while line:
-       operations.append(line)
-       executed.append(False)
-       switched.append(False)
+       number = int(line)
+
+       if len(window) < windowLen:
+           window.append(number)
+       else:
+           numbers.append(number)
+
        line = fp.readline()
-   for i in range(0,len(executed)):
-       oldOperation = operations[i]
-       opType = operations[i][:3]
-       if opType != "acc":
-           # print(opType)
-           if opType == "nop":
-               newOpType = "jmp"
-           elif opType == "jmp":
-               newOpType = "nop"
-           newOperation = newOpType + operations[i][3:]
-           #print(newOperation)
-           operations[i] = newOperation
-           # print("Starting New")
-           # print("----")
-           #print(operations)
-           testPc = RunLoop(operations)
-           if testPc == len(operations):
-               break
-           else:
-               operations[i] = oldOperation
 
-
-
-
-print(accumulator)
+   for x in numbers:
+       print(window)
+       valid = False
+       for w1 in window:
+           for w2 in window:
+               if w1 != w2:
+                   if (w1 + w2) == x:
+                       valid = True
+       if valid == False:
+           print("Invalid Num: {}".format(x))
+           break
+       else:
+           window.pop(0)
+           window.append(x)
