e47b75d30b3490da8bcd021ea8564f0f3c88ff03
Matt Johnson <>
2020-12-17 16:43:00 -0500

    day 17 wip

--- 2020/day17/puzzle_day_17_01.py
+++ 2020/day17/puzzle_day_17_01.py
@@ -3,70 +3,28 @@ start_time = time.time()
 
 filepath = 'test'
 
-rules = []
+startingGrid = []
 
-processState = 0
-nextState = 0
-ticketErrRate = 0
-validTickets = []
+x = 0
+y = 0
+z = 0
+
 
 with open(filepath) as fp:
    line = fp.readline()
    cnt = 1
    while line:
-
-       # print("Line: {}".format(line))
-       if line.isspace():
-           processState = processState + 1
-           # print("NextState: {}".format(processState))
-       else:
-           if processState == 0:
-               lineItems = line.split()
-               lineRule = []
-               lineRule.append(lineItems[0])
-               print(lineItems)
-               for lineItem in lineItems:
-                   if(lineItem.find('-') != -1):
-                       lineRuleRange = []
-                       lineItemRanges = lineItem.split('-')
-                       for lineItemRange in lineItemRanges:
-                           lineRuleRange.append(int(lineItemRange))
-                       lineRule.append(lineRuleRange)
-               rules.append(lineRule)
-
-           # if processState == 1:               
-           #     if(line.find("ticket") == -1):
-           #         ticketNum = line.split("\n")[0]
-           #         validTickets.append
-
-
-           if processState == 2:
-               ticketNums = []
-               if(line.find("ticket") == -1):
-                   #print(line.split("\n")[0].split(','))
-                   for ticketNum in line.split("\n")[0].split(','):
-                       # ticketNums.append(int(ticketNum))
-                       num = int(ticketNum)
-                       check = False
-                       for rule in rules:
-                           if num in range(rule[1][0],rule[1][1] + 1):
-                               check = True
-                               break
-                           elif num in range(rule[2][0],rule[2][1] + 1):
-                               check = True
-                               break
-                       if check == False:
-                           print(num)
-                           ticketErrRate = ticketErrRate + num
-
-
-
+       line = line.split("\n")[0]
+       row = []
+       for c in line:
+           row.append(c)
+       startingGrid.append(row)
 
        line = fp.readline()
 
-print("    ")
+print(startingGrid)
 
-print(ticketErrRate)
+
 
 print("--- %s seconds ---" % (time.time() - start_time))
 
