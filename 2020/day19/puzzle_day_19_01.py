f708d04425cc69a7aa8bfaef5782f55231286ec1
Matt Johnson <>
2020-12-19 10:52:02 -0500

    finished day19 p1

---
+++ 2020/day19/puzzle_day_19_01.py
@@ -0,0 +1,100 @@
+import time
+start_time = time.time()
+
+filepath = 'input'
+
+state = 0
+nextState = 0
+rules = {}
+
+def checkRule(checkString, rules, ruleToCheck):
+    # print("checkRule->checkString: {}".format(checkString))
+    ruleCheck = [False, checkString]
+    rule = rules[ruleToCheck]
+    tmpCheckString = checkString
+    for subRule in rule:
+        for subSubRule in subRule:
+            ruleCheck[0] = False
+            if subSubRule.isdigit():
+                ruleCheck = checkRule(tmpCheckString, rules, subSubRule)
+                if ruleCheck[0] == False:
+                    break
+                else:
+                    tmpCheckString = ruleCheck[1]
+            else:
+                if tmpCheckString[0] == subSubRule:
+                    ruleCheck[0] = True
+                    tmpCheckString = tmpCheckString[1:]
+                else:
+                    ruleCheck[0] = False
+                    break
+
+                # pos = tmpCheckString.find(subSubRule)
+                # if pos > -1:
+                #     ruleCheck[0] = True
+                #     tmpCheckString = checkString[pos+1:]
+                # else:
+                #     ruleCheck[0] = False
+                #     break
+
+
+        if ruleCheck[0] == True:
+            ruleCheck[1] = tmpCheckString
+            break
+        else:
+            tmpCheckString = checkString
+
+    return ruleCheck
+
+
+
+
+with open(filepath) as fp:
+    line = fp.readline()
+    cnt = 0
+    while line:
+        if state == 0:
+            if line.isspace():
+                nextState = state + 1
+                print("Rules:")
+                print(rules)
+                print("Done with rules")
+            else:
+                rule = line.split("\n")[0].split(":")
+                ruleIdx = rule[0]
+                subrules = rule[1].replace("\"","").split("|")
+
+                # rule.append(ruleIdx)
+
+                tmpSubRules = []
+                for subRule in subrules:
+                    subRule = subRule.split()
+                    tmpSubRules.append(subRule)
+                    #print("subrule {}".format(subRule))
+
+                rules[ruleIdx] = tmpSubRules
+                # rule.append(tmpSubRules)
+
+                print(tmpSubRules)
+                #rules.append(rule)
+        elif state == 1:
+            checkString = line.split("\n")[0]
+            print("     ")
+            print("Checkstring: {}".format(checkString))
+            result = checkRule(checkString, rules, '0')
+            if result[0] == True:
+                if len(result[1]) == 0:
+                    cnt = cnt + 1
+            # print("Checkstring End: {}".format(checkString))
+            # print("Checkstring: {}".format(checkString))
+            # for rule in rules:
+            #     print("New Rule")
+            #     for subRule in rule:
+            #         print(subRule)
+
+        state = nextState
+
+        line = fp.readline()
+
+print("Cnt: {}".format(cnt))
+print("--- %s seconds ---" % (time.time() - start_time))
